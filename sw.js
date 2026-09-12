13d9ac753ed7f4a8d39763336ba96bcf310b4dfd
Steffen Rentschler <54315384+hstre@users.noreply.github.com>
2026-09-12 13:39:18 -0700

    Refresh offline cache after diagram fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "eins-bis-zehn-v2";
+const CACHE = "eins-bis-zehn-v3";
 const FILES = ["./", "./index.html", "./styles.css", "./sets.js", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
