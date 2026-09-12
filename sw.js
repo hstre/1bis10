fab1ce55f0ea0ecdee0085a295132c4d42d370a6
Steffen Rentschler <54315384+hstre@users.noreply.github.com>
2026-09-12 14:15:58 -0700

    Cache-Version aktualisieren (sw.js)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "eins-bis-zehn-v8";
-const FILES = ["./", "./index.html", "./styles.css?v=8", "./sets.js?v=8", "./app.js?v=8", "./manifest.webmanifest"];
+const CACHE = "eins-bis-zehn-v9";
+const FILES = ["./", "./index.html", "./styles.css?v=9", "./sets.js?v=9", "./app.js?v=9", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
